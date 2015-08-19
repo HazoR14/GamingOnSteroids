4df294d42cee2025f94fb2808deaa2878c280ccd
funboxxx <funboxxx@wp.pl>
2015-08-19 07:48:33 +0100

    1.0.1

--- Scripts/Common/FunboxAIO.lua
+++ Scripts/Common/FunboxAIO.lua
@@ -1,7 +1,7 @@
 --
 --
 --   F U N B O X    A  I  O
---           1 . 0
+--        1 . 0  . 1
 --
 -- L E B L A N C
 if GetObjectName(GetMyHero()) == "Leblanc" then
@@ -10,7 +10,7 @@ if GetObjectName(GetMyHero()) == "Leblanc" then
     local t = GetTarget(850)
     local pw = GetPredictionForPlayer(GetOrigin(GetMyHero()), t, GetMoveSpeed(t), 2000, 500, 850, 120, false, true)
     local pe = GetPredictionForPlayer(GetOrigin(GetMyHero()), t, GetMoveSpeed(t), 2000, 500, 850, 55, true, true)
-    if KeyIsDown(0x20) and ValidTarget(t, 850) then
+    if KeyIsDown(0x20) and ValidTarget(t, 850) and t ~=nil then
       if CanUseSpell(myHero, _R) == 5 or CanUseSpell(myHero, _R) == 2 then
         if CanUseSpell(myHero, _Q) == 0 then
           CastTargetSpell(t, _Q)
@@ -39,15 +39,15 @@ if GetObjectName(GetMyHero()) == "Lucian" then
   OnLoop(function(myHero)
     local t = GetTarget(675)
     local pw = GetPredictionForPlayer(GetOrigin(GetMyHero()), t, GetMoveSpeed(t), 2000, 300, 1000, 80, false, true)
-    if KeyIsDown(0x20) and GotBuff(myHero, "lucianpassivebuff") == 1 then
-      AttackUnit(t)
-    end
-    if KeyIsDown(0x20) and ValidTarget(t, 675) then
+    if KeyIsDown(0x20) and ValidTarget(t, 675) and t ~=nil then
+      if GotBuff(myHero, "lucianpassivebuff") == 1 then
+        AttackUnit(t)
+      end
       if CanUseSpell(myHero, _Q) == 0 then
         CastTargetSpell(t, _Q)
       elseif CanUseSpell(myHero, _W) == 0 then
         DelayAction(function()
-          if GotBuff(myHero, "lucianpassivebuff") == 0 then
+          if GotBuff(myHero, "lucianpassivebuff") == 0 and pw.HitChance == 1 then
             CastSkillShot(_W, pw.PredPos.x, pw.PredPos.y, pw.PredPos.z)
           end
         end, 500)
@@ -64,39 +64,57 @@ if GetObjectName(GetMyHero()) == "Rengar" then
     local taa = GetTarget(240)
     local pef = GetPredictionForPlayer(GetOrigin(GetMyHero()), ts, GetMoveSpeed(ts), 1600, 250, 1000, 60, true, true)
     local pet = GetPredictionForPlayer(GetOrigin(GetMyHero()), tc, GetMoveSpeed(tc), 1600, 250, 350, 60, true, true)
-    if GetCurrentHP(myHero) < 500 and GetCurrentMana(myHero) == 5 then
+    if (GetCurrentHP(myHero)/GetMaxHP(myHero))*100 <= 40 and GetCurrentMana(myHero) == 5 then
       CastSpell(_W)
     end
     if KeyIsDown(0x20) then
-      if GotBuff(myHero, "rengarqbase") == 1 or GotBuff(myHero, "rengarqemp") == 1 or GotBuff(myHero, "rengarwbuff") == 1 or GotBuff(ts, "rengareslow") == 1 or GotBuff(ts, "RengarEFinalMAX") == 1 then
-        AttackUnit(taa)
+      if taa ~=nil then
+        if GotBuff(myHero, "rengarqbase") == 1 or GotBuff(myHero, "rengarqemp") == 1 then
+          AttackUnit(taa)
+        end
+        if GotBuff(ts, "rengareslow") == 1 or GotBuff(ts, "RengarEFinalMAX") == 1 then
+          DelayAction(function()
+            AttackUnit(taa)
+          end, 500)
+        end
       end
       if GetCurrentMana(myHero) <= 4 then
-        if ValidTarget(ts, 900) and CanUseSpell(myHero, _Q) == 0 and GotBuff(myHero, "rengarpassivebuff") == 1 then
+        if ts ~=nil and ValidTarget(ts, 900) and CanUseSpell(myHero, _Q) == 0 and GotBuff(myHero, "rengarpassivebuff") == 1 then
           CastSpell(_Q)
         end
-        if CanUseSpell(myHero, _Q) == 0 and ValidTarget(tc, 250) then
-          CastSpell(_Q)
-        end
-        if CanUseSpell(myHero, _W) == 0 and ValidTarget(tc, 350) then
-          CastSpell(_W)
-        end
-        if CanUseSpell(myHero, _E) == 0 and ValidTarget(tc, 350) then
-          CastSkillShot(_E, pet.PredPos.x, pet.PredPos.y, pet.PredPos.z)
-        end
-        if GotBuff(myHero, "rengarpassivebuff") == 0 then
-          if CanUseSpell(myHero, _E) == 0 and ValidTarget(ts, 1000) then
-            CastSkillShot(_E, pef.PredPos.x, pef.PredPos.y, pef.PredPos.z)
+        if CanUseSpell(myHero, _Q) == 0 then
+          if tc ~=nil and ValidTarget(tc, 250) then
+            CastSpell(_Q)
+          end
+        elseif CanUseSpell(myHero, _W) == 0 then
+          if tc ~=nil and ValidTarget(tc, 350) then
+            CastSpell(_W)
+          end
+        elseif CanUseSpell(myHero, _E) == 0 then
+          if tc ~=nil and ValidTarget(tc, 350) and pet.HitChance == 1 then
+            CastSkillShot(_E, pet.PredPos.x, pet.PredPos.y, pet.PredPos.z)
+          end
+          if GotBuff(myHero, "rengarpassivebuff") == 0 then
+            if ts ~=nil and ValidTarget(ts, 1000) and pef.HitChance == 1 then
+              CastSkillShot(_E, pef.PredPos.x, pef.PredPos.y, pef.PredPos.z)
+            end
           end
         end
       end
       if GetCurrentMana(myHero) == 5 then
-        if GetCurrentHP(myHero) >= 500 then
-          if ValidTarget(tc, 250) then
-            CastSpell(_Q)
+        if (GetCurrentHP(myHero)/GetMaxHP(myHero))*100 > 40 then
+          if tc ~=nil and GetDistance(tc, myHero) < 300 then
+            if ValidTarget(tc, 250) then
+              CastSpell(_Q)
+            end
+          end
+          if ts ~=nil and GetDistance(ts, myHero) >= 300 then
+            if ValidTarget(ts, 1000) and pef.HitChance == 1 then
+              CastSkillShot(_E, pef.PredPos.x, pef.PredPos.y, pef.PredPos.z)
+            end
           end
           if GotBuff(myHero, "rengarpassivebuff") == 1 then
-            if ValidTarget(ts, 800) then
+            if ts ~=nil and ValidTarget(ts, 800) then
               CastSpell(_Q)
             end
           end
